18495b52986c829cf3d1111eaa8c22cf34bc3d08
retamar <angel.retamar@gmail.com>
2022-12-04 09:02:50 +0100

    Day4.Quiz1. Overlapping ranges

--- src/day4/day4quiz1.groovy
+++ src/day4/day4quiz1.groovy
@@ -10,7 +10,9 @@ def parseRange = {String rangeToBeParsed ->
 	
 }
 
-def line = "37-87,36-87"
+def line = "2-8,13-14"
 def rangeListToBeParsed = line.split(",")
 def elfRanges = rangeListToBeParsed.collect {parseRange(it)}
-println elfRanges
+
+boolean overlapped = elfRanges[0].containsAll(elfRanges[1]) || elfRanges[1].containsAll(elfRanges[0])
+println overlapped
