015b761f5f75d07ffdb99d8e48bf5c6bb1cea70d
retamar <angel.retamar@gmail.com>
2022-12-12 09:56:45 +0100

    Day11. Quiz2. Some cleaning

--- src/day11/day11quiz2.groovy
+++ src/day11/day11quiz2.groovy
@@ -4,37 +4,24 @@ int rounds = 10000
 
 
 def monkeys = []
-def divider = []
+def divider = 1
 File input = "docs/day11.txt" as File
-MonkeysFactory monkeysFactory = new MonkeysFactory(boredMonkeyClosure:{it % divider[0]})
+MonkeysFactory monkeysFactory = new MonkeysFactory(boredMonkeyClosure:{it % divider})
 monkeys = monkeysFactory.parse(input)
 
-int dividerValue = 1;
-monkeys.each { 
-	dividerValue = dividerValue*it.test.divisible
-}
-divider[0] = dividerValue
 
-println divider[0]
+monkeys.each { 
+	divider = divider*it.test.divisible
+}
+
+
 println monkeys
 
 (1..rounds).each{int round ->
 	monkeys.each {Monkey monkey ->
 		monkey.doMonkeyTurns(monkeys)
 	}
-	if (round == 1) {
-		println "ROUND $round"
-		println monkeys.inspectionTimes
-	}
-	if (round == 20) {
-		println "ROUND $round"
-		println monkeys.inspectionTimes
-	}
-	if (round % 1000 == 0) {
-		println "ROUND $round"
-		println monkeys.inspectionTimes
-		println monkeys.items
-	}
+
 }
 
 def mostActiveMonkeys = monkeys.sort{it.inspectionTimes}
