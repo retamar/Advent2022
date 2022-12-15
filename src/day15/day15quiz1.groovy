676c7ebfda0fd7c304f0860251c6f357f92804a6
retamar <angel.retamar@gmail.com>
2022-12-15 09:56:18 +0100

    Day15. Parsed imput

---
+++ src/day15/day15quiz1.groovy
@@ -0,0 +1,26 @@
+package day15
+
+File input = "docs/day15test.txt" as File
+
+
+def sensors = []
+
+input.eachLine { line ->
+	
+	def sensorAndBeaconConfiguration = line.tokenize(":")
+	
+	def parsedSensorData = (sensorAndBeaconConfiguration[0] - "Sensor at ").tokenize(",")
+	int sensorX = ((parsedSensorData[0]-"x=").trim()) as Integer
+	int sensorY = ((parsedSensorData[1]-"y=").trim()) as Integer
+	
+	def parsedBeaconData = (sensorAndBeaconConfiguration[1] - " closest beacon is at ").tokenize(",")
+	int beaconX = ((parsedBeaconData[0]-"x=").trim()) as Integer
+	int beaconY = ((parsedBeaconData[1]-"y=").trim()) as Integer
+	
+	sensors << [sensor:[x:sensorX, y:sensorY], beacon:[x:beaconX, y:beaconY]]
+	
+}
+
+
+println sensors
+
