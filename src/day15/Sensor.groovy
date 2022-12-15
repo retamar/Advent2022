258c6cc9e94b4797121f5a968acdd3c963d15c1e
retamar <angel.retamar@gmail.com>
2022-12-15 10:41:39 +0100

    Da15. Quiz1. Solved with test data...

--- src/day15/Sensor.groovy
+++ src/day15/Sensor.groovy
@@ -15,11 +15,29 @@ class Sensor {
 		this.sensorX = sensorX
 		this.sensorY = sensorY
 		this.beaconX = beaconX
-		this.beaconX = beaconY
+		this.beaconY = beaconY
 	}
 	
 	int getDistanceToBeacon() {		
 		return Math.abs(sensorX-beaconX)+Math.abs(sensorY-beaconY)		
+	}
+	
+	def notBeaconPointsAtRow(int row) {
+		int distanceToRow = Math.abs(sensorY - row)
+		if (distanceToRow > this.distanceToBeacon) {
+			return []
+		}
+		
+		
+		int xWidth = this.distanceToBeacon - distanceToRow
+		def points = []
+		(this.sensorX-xWidth..this.sensorX+xWidth).each{
+			points << [it,row]
+		}
+		
+		println "${this} -> $points -> ${points.size()} -> $distanceToBeacon"
+		
+		return points
 	}
 	
 	String toString() {
--- src/day15/day15quiz1.groovy
+++ src/day15/day15quiz1.groovy
@@ -4,6 +4,7 @@ File input = "docs/day15test.txt" as File
 
 
 def sensors = []
+def beacons = []
 
 input.eachLine { line ->
 	
@@ -13,19 +14,25 @@ input.eachLine { line ->
 	int sensorX = ((parsedSensorData[0]-"x=").trim()) as Integer
 	int sensorY = ((parsedSensorData[1]-"y=").trim()) as Integer
 	
-	def parsedBeaconData = (sensorAndBeaconConfiguration[1] - " closest beacon is at ").tokenize(",")
+	def parsedBeaconData = (sensorAndBeaconConfiguration[1] - " closest beacon is at ").tokenize(",")	
 	int beaconX = ((parsedBeaconData[0]-"x=").trim()) as Integer
 	int beaconY = ((parsedBeaconData[1]-"y=").trim()) as Integer
 	
 	sensors << new Sensor(sensorX, sensorY, beaconX, beaconY)
+	beacons << [beaconX, beaconY]
 	
 }
 
 println sensors
 
 int rowToBeTested = 10
-
-sensors.each {
-	
+def notBeaconPoints = ([] as Set)
+sensors.each {Sensor sensor->
+	notBeaconPoints.addAll(sensor.notBeaconPointsAtRow(rowToBeTested))
 }
+
+notBeaconPoints.removeAll(beacons)
+
+println notBeaconPoints
+println notBeaconPoints.size()
 
